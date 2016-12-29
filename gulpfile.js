6ca5d014f7c40f8d5336301676e57bdbba66d7dc
Andrew <andrewboudreau@gmail.com>
2016-12-29 14:00:36 -0600

    css logo with custom font
    using scss with gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var webserver = require('gulp-webserver');
  
 gulp.task('webserver', function() {
@@ -9,3 +10,18 @@ gulp.task('webserver', function() {
       open: true
     }));
 });
+
+gulp.task('styles', function() {
+    gulp.src('wwwroot/css/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css/'));
+});
+
+gulp.task('watchCss',function() {
+    gulp.watch('wwwroot/css/**/*.scss',['styles']);
+});
+
+
+gulp.task('build', ['styles']);
+
+gulp.task('debug', ['watchCss', 'webserver']);
